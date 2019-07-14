f144d4faf6d90ca10b87db4ed65585309a86af4a
Jay Maas <jaymaas.dev@gmail.com>
2019-07-13 23:41:53 -0600

    Sockets Abandon
    Current impl. of sockets abandoned

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 const express = require('express');
 const cors = require('cors')
 const server = express();
-const http = require('http').createServer(server)
-const io = require('socket.io')(http)
-
 
 server.use(cors())
 server.use(express.json())
 
 server.get('/', (req, res) => {
+    console.log('hit')
     res.json({ message: `The server is on and waiting for requests...`})
 })
-
-io.on('connection', socket => {
-    console.log('A user connected.');
-  
-    socket.on('sendMessage', msg => {
-      socket.emit('sendMessage', msg);
-    });
-  
-    socket.on('disconnect', () => {
-      console.log('A user disconnected.');
-    });
-  });
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** server up on port ${port} **\n`))
